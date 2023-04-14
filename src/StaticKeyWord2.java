fd2575f73418cd49d96170ff79a85b983991281c
Vlad2034 <leevb@yandex.ru>
2023-04-13 18:54:57 -0700

    JavaCourseRu

--- src/StaticKeyWord2.java
+++ src/StaticKeyWord2.java
@@ -1,14 +1,35 @@
 public class StaticKeyWord2 {
-   public static void main(String [] args) {
-        HumanSt1.description = "Nice"; //не надо создавать объект класса HumanSt1
-       // мы получаем прямой доступ к переменной
-       HumanSt1.getDescription();
-       HumanSt1.description = "Bad";
+    public static void main(String[] args) {
+        // static методы и переменные нужны если
+        // 1. если методу не нужно знать переменные объекты
+        // например: метод Math - там много статических методов
+        // к которым мы можем получить доступ не создавая объекта
+        System.out.println(Math.pow(2,4));
+        System.out.println(Math.PI);
+        //
+
+        HumanSt1 h111 = new HumanSt1("Bob", 40);
+        HumanSt1 h222 = new HumanSt1("Tom", 30);
+        HumanSt1 h333 = new HumanSt1("Bill", 50);
+
+        h111.printNumberOfPeople();
+        h222.printNumberOfPeople();
+        h333.printNumberOfPeople();
+
+//        HumanSt1.description = "Nice"; //не надо создавать объект класса HumanSt1
+//        // мы получаем прямой доступ к переменной
+//        h111.getAllFields();
+//        h222.getAllFields();
+//        HumanSt1.description = "Bad";
+//        h111.getAllFields();
+//        h222.getAllFields();
+
 
 //       HumanSt1 h111 = new HumanSt1("Bob", 50);
 //       h111.description = "Bad";
-       //doesn't work
-   }
+        //doesn't work
+    }
+}
 class HumanSt1 {
         //переменные объекта - они могут быть только у объекта и не могут быть у класса
         private String name;
@@ -16,10 +37,13 @@ class HumanSt1 {
         public static String description; //статическая переменная принадлежит классу HumanSt1
         // а не объекту. Она будет делиться между объектами этого класса
 
+        public static int countPeople; //zero по умолчанию
+
+
         public HumanSt1 (String name, int age) {
-            System.out.println("привет из третьего конструктора");
             this.age = age;
             this.name = name;
+            countPeople++;
         }
 
         // методы объекта
@@ -30,7 +54,20 @@ class HumanSt1 {
         public static void getDescription(){
             System.out.println(description);
         }
-    }
 
+        public void getAllFields(){
+            System.out.println(name + ", " + age + ", " + description);
+        }
 
+//        public static void printAllFields(){
+//            System.out.println(name);
+//        } //static не может работать с изменяющимися значениями объекта, потому что
+            // они разные у каждого объекта
+
+        public void printNumberOfPeople(){
+            System.out.println("Number of people = " +countPeople);
+        }
 }
+
+
+
